6a8773ad0649a2557904579fd04b640fd16d3b5d
Aditya Inapurapu <adityaii@gmail.com>
2021-02-18 21:04:30 -0800

    first-test.spec.ts

---
+++ src/app/first-test.spec.ts
@@ -0,0 +1,17 @@
+describe('my first test', () => {
+    let sut;
+
+    beforeEach(() => {
+        sut = {}
+    })
+
+    it('should be true if true', () => {
+        sut.a = false;
+
+        // act
+        sut.a = true;
+
+        // assert
+        expect(sut.a).toBe(true);
+    })
+})
